6a66965c165192b3001834c9eb427859aeb36852
Ludovico Jommi <l.jommi@plasgroup.it>
2026-04-14 12:45:19 +0200

    fix: aggiunto tasto toggle emergenza su UI mobile

--- components/LeadTable.tsx
+++ components/LeadTable.tsx
@@ -189,6 +189,15 @@ const LeadTable: React.FC<LeadTableProps> = ({ leads, onUpdateLeadField, onLinkL
                     
                     <div className="flex items-center gap-1 z-10">
                         <button
+                            onClick={() => onUpdateLeadField?.(lead.id, 'emergenza', !lead.emergenza)}
+                            className={`p-2 rounded-lg transition-colors border shadow-sm ${lead.emergenza && !lead.orientamento_effettuato ? 'bg-red-500 text-white border-red-500 animate-pulse' : 'bg-white border-gray-200 text-gray-400 hover:text-red-500'}`}
+                            title="Segnala Emergenza"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                            </svg>
+                        </button>
+                        <button
                             onClick={() => handleToggleBlocca(lead)}
                             className={`p-2 rounded-lg transition-colors border shadow-sm ${lead.bloccato ? 'border-white/30 text-white bg-black/20' : 'bg-white border-gray-200 text-gray-400'}`}
                         >
